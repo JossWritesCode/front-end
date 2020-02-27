2d98778eddb9292488d103b798b5c93270311664
Victor Tran <victor.n.tran@outlook.com>
2020-02-27 18:53:41 -0500

    merge my changes

--- src/components/index.js
+++ src/components/index.js
@@ -6,9 +6,6 @@ import AuthOptionsModal from "./Modal/AuthOptionsModal/AuthOptionsModal";
 import SideDrawerButton from "./SideDrawer/SideDrawerButton/SideDrawerButton";
 import SideDrawerMenu from "./SideDrawer/SideDrawerMenu/SideDrawerMenu";
 
-<<<<<<< HEAD
-export { AuthModal, Navbar, Backdrop, Modal, AuthOptionsModal };
-=======
 export {
   AuthModal,
   Navbar,
@@ -18,4 +15,3 @@ export {
   SideDrawerButton,
   SideDrawerMenu
 };
->>>>>>> e1230b3d79db77260334456f0f15cbc047444970
