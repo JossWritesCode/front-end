d70d0a729c040648acbd239176d7d4523d41538d
Victor Tran <victor.n.tran@outlook.com>
2020-02-27 19:39:28 -0500

    Create LoginPage component

---
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../";
+const LoginPage = ({ className }) => {
+  return (
+    <div className={`${className} LoginPage`}>
+      <Header title="Login" />
+    </div>
+  );
+};
+
+export default LoginPage;
