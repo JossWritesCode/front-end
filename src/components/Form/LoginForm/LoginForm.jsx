3d0fd307c477741cad2654a7667ea25cd5294a5c
Victor Tran <victor.n.tran@outlook.com>
2020-02-28 16:24:04 -0500

    finish integrating formik into login

--- src/components/Form/LoginForm/LoginForm.jsx
+++ src/components/Form/LoginForm/LoginForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import { SubmitButton } from "../../";
+
 const LoginForm = ({ className = "", touched, errors }) => {
   return (
     <div className={`${className} FormContainer`}>
@@ -9,23 +10,38 @@ const LoginForm = ({ className = "", touched, errors }) => {
         <label>
           Email
           <Field type="email" name="email" />
+          <p>{touched.email && errors.email}</p>
         </label>
         <label>
           Password
           <Field type="password" name="password" />
+          <p>{touched.password && errors.password}</p>
         </label>
 
-        <SubmitButton />
+        <SubmitButton
+          disabled={
+            Object.keys(touched).length === 0 || Object.keys(errors).length > 0
+          }
+        />
       </Form>
     </div>
   );
 };
 
 const enhanceForm = withFormik({
-  mapPropsToValues(props) {
-    return {};
+  mapPropsToValues({ email = "", password = "" }) {
+    return { email, password };
   },
-  validationSchema: Yup.object().validate({})
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email("Please enter valid email"),
+    password: Yup.string()
+      .required("Please enter the required field")
+      .min(8, null)
+  }),
+  handleSubmit({ email, password }, { resetForm }) {
+    // axios call here
+    resetForm();
+  }
 });
 
 export default enhanceForm(LoginForm);
