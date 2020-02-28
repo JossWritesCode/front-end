8bf4bf8829dd4d35004142f6c48de0cad76cea75
Conary36 <cbeckford144@gmail.com>
2020-02-27 21:52:31 -0500

    updated donatePage

--- src/components/DonatePage/DonationPage.jsx
+++ src/components/DonatePage/DonationPage.jsx
@@ -3,15 +3,14 @@ import Header from '../Header/Header'
 import {connect} from 'react-redux';
 import {initialState, rootReducer} from '../../redux/reducers/reducer'
 import {donateMoney} from '../../redux/actions/action'
+//import {useSelector} and {useDispatch} from react-redux
+
 
 const DonationPage = (props) => {
      const [state, dispatch] = useReducer(rootReducer, initialState);
-    
+    //const state = useSelctor(state => state.InitialState)
+    //const donateMoney = useDispatch()   then write inline function...
     console.log(props);
-
-    // function five(){
-    //     setData(data )
-    // }
 
     
 
