e49f4c26db8bfa50c4ff7105c9a42c20186f09e5
Carlos Turcios <turcios50@gmail.com>
2020-02-27 18:45:08 -0800

    fixed ProtectedRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Navbar, SideDrawerMenu, AuthModal } from '../';
-import PrivateRoute from '../utils/ProctectedRoute';
+import ProtectedRoute from '../utils/ProctectedRoute';
+import LoginPage from '../Pages/LoginPage/LoginPage';
 function App() {
   const [modalsVisibility, setModalVisibility] = useState({
     authModal: false,
@@ -22,6 +23,7 @@ function App() {
       setModalVisibility(newModalVisibilityState);
     }
   };
+
   return (
     <div className='App'>
       <AuthModal
@@ -33,7 +35,7 @@ function App() {
         show={modalsVisibility.sideDrawerMenu}
       />
       <Navbar visibilityHandler={modalsVisibilityHandler} />
-      <PrivateRoute path='/' component={} />
+      <ProtectedRoute path='/' component={LoginPage} />
     </div>
   );
 }
