69e26a0237df2174b82fb7c9071755d4ace73409
Victor Tran <victor.n.tran@outlook.com>
2020-02-28 16:15:40 -0500

    add disabled button feature

--- src/components/Button/SubmitButton/SubmitButton.jsx
+++ src/components/Button/SubmitButton/SubmitButton.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Button } from "../..";
 
-const SubmitButton = ({ className = "PrimaryButton" }) => {
-  return <Button text="Submit" className={className} />;
+const SubmitButton = ({ className = "PrimaryButton", disabled = false }) => {
+  return <Button text="Submit" className={className} disabled={disabled} />;
 };
 
 export default SubmitButton;
