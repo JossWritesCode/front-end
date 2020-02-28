018140711144b5f3b6bc76027ccad738d02a6ac4
Victor Tran <victor.n.tran@outlook.com>
2020-02-28 15:47:10 -0500

    rename and move

--- src/components/Pages/SignUpPage/SignUpPage.jsx
+++ src/components/Pages/SignUpPage/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, SignUpForm, OAuthOptions } from "../";
+import { Header, SignUpForm, OAuthOptions } from "../..";
 
 const SignUp = ({ className }) => {
   return (
