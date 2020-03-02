8802834f91c68416ca6f5ab98f685357dcef8481
Victor Tran <victor.n.tran@outlook.com>
2020-03-01 20:51:15 -0500

    update UI/X on what should happen when buttons are clicked or when amount is entered

--- src/components/Form/DonateForm/DonateForm.jsx
+++ src/components/Form/DonateForm/DonateForm.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { withFormik, Field, Form } from "formik";
 import * as Yup from "yup";
-import { Button, DonateButton, ConfirmationModalContainer } from "../../";
+import { connect } from "react-redux";
+import { Button, DonateButton } from "../../";
+
+import { updateAmount } from "../../../redux/actionCreators/donationCreators";
 
 const DonateForm = ({
   buttons,
@@ -14,7 +17,9 @@ const DonateForm = ({
   visibilityHandler
 }) => {
   useEffect(() => {
-    resetButtons();
+    if (values.amount !== "" || values.amount.toString().length > 0) {
+      resetButtons();
+    }
   }, [values.amount]);
   const amountButtons = Object.keys(buttons).map(key => {
     const object = buttons[key];
@@ -23,7 +28,9 @@ const DonateForm = ({
         type="button"
         text={`$${object.amount}`}
         className={`DonationAmountButton ${
-          object.active ? "PrimaryButton" : ""
+          object.active || object.amount === values.amount
+            ? "PrimaryButton"
+            : ""
         }`}
         key={key}
         clickHandler={clickHandler}
@@ -39,7 +46,18 @@ const DonateForm = ({
         <div>{amountButtons}</div>
         <label>
           Other Amount
-          <Field name="amount" type="tel" placeholder="$ Other amount" />
+          {/* clicking on a button should clear state */}
+          <Field
+            name="amount"
+            type="tel"
+            placeholder="$ Other amount"
+            value={
+              values.amount === 0 ||
+              Object.keys(buttons).some(button => buttons[button].active)
+                ? ""
+                : values.amount
+            }
+          />
           <p>{touched.amount && errors.amount}</p>
         </label>
         <label className="box">
@@ -57,17 +75,39 @@ const enhanceForm = withFormik({
     return { amount, monthly };
   },
   validationSchema: Yup.object().shape({
-    amount: Yup.string().matches(/^[\d]*$/gi, "Only numbers go here")
+    amount: Yup.string().matches(
+      /^[\d]*\.[\d]{2}$/gi,
+      "Please enter something like: 123.00 or 123.45"
+    )
   }),
   handleSubmit(
     { amount, monthly },
-    { props: { buttons, resetButtons, visibilityHandler }, resetForm }
+    {
+      props: {
+        buttons,
+        resetButtons,
+        visibilityHandler,
+        updateAmount,
+        amount: reduxAmount
+      },
+      resetForm
+    }
   ) {
     // so this is what's going to happen, donation form simply passes data back into redux store to store the amount. The confirmation modal is another form
+    const donationAmount = amount ? amount : reduxAmount;
+    updateAmount(donationAmount);
     visibilityHandler(null, "confirmationModal");
-    resetForm();
-    // resetButtons(); will be used inside the modal or not reset at all
   }
 });
+const mapStateToProps = ({
+  user: {
+    donation: { amount, monthly }
+  }
+}) => ({ amount, monthly });
 
-export default enhanceForm(DonateForm);
+const mapDispatchToProps = { updateAmount };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(enhanceForm(DonateForm));
