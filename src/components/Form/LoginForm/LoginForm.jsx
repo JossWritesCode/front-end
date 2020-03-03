0697fc388895b2158db569048ea932a0a027a413
Carlos Turcios <turcios50@gmail.com>
2020-03-02 21:24:34 -0800

    updated state

--- src/components/Form/LoginForm/LoginForm.jsx
+++ src/components/Form/LoginForm/LoginForm.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
-import { SubmitButton } from "../../";
-import { login } from "../../../redux/actionCreators";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import { compose } from "redux";
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import { SubmitButton } from '../../';
+import { login } from '../../../redux/actionCreators';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'redux';
 
-const LoginForm = ({ className = "", touched, errors, authError }) => {
+const LoginForm = ({ className = '', touched, errors, authError }) => {
   return (
     <div className={`${className} FormContainer`}>
-      <Form className="Form">
+      <Form className='Form'>
         <label>
           Email
-          <Field type="email" name="email" />
+          <Field type='email' name='email' />
           <p>{touched.email && errors.email}</p>
         </label>
         <label>
           Password
-          <Field type="password" name="password" />
+          <Field type='password' name='password' />
           <p>{touched.password && errors.password}</p>
         </label>
 
@@ -35,13 +35,13 @@ const LoginForm = ({ className = "", touched, errors, authError }) => {
 const enhanceForm = compose(
   withRouter,
   withFormik({
-    mapPropsToValues({ email = "", password = "" }) {
+    mapPropsToValues({ email = '', password = '' }) {
       return { email, password };
     },
     validationSchema: Yup.object().shape({
-      email: Yup.string().email("Please enter valid email"),
+      email: Yup.string().email('Please enter valid email'),
       password: Yup.string()
-        .required("Please enter the required field")
+        .required('Please enter the required field')
         .min(8, null)
     }),
     async handleSubmit(
@@ -52,7 +52,7 @@ const enhanceForm = compose(
       login(credentials)
         .then(() => {
           resetForm();
-          history.push("/donate");
+          history.push('/donate');
         })
         .catch(err => console.log(err));
     }
