4dc7583e256353c7530f53cde3a40d003d08f02a
Victor Tran <victor.n.tran@outlook.com>
2020-03-05 21:52:26 -0500

    remove outer state prop

--- src/components/Modal/LoadingModal/LoadingModal.jsx
+++ src/components/Modal/LoadingModal/LoadingModal.jsx
@@ -14,6 +14,6 @@ const LoadingModal = ({ phase, active }) => {
   );
 };
 
-const mapStateToProps = ({ loading: { phase, active } }) => ({ phase, active });
+const mapStateToProps = ({ auth: {loading: { phase, active }} }) => ({ phase, active });
 
 export default connect(mapStateToProps)(LoadingModal);
